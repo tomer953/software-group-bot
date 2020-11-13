e8310532adfe230ecde823c86bde40e4e18ba535
tomer953 <tomer953@gmail.com>
2020-11-13 17:32:35 +0200

    log GMT, update version

--- src/controllers/schedulars.controller.ts
+++ src/controllers/schedulars.controller.ts
@@ -12,12 +12,12 @@ export class BotSchedulars {
 }
 
 export async function initSchedulars() {
-    console.log('initSchedulars');
     for (const job of BotSchedulars.jobs) {
         job.cancel();
     }
     BotSchedulars.jobs = [];
     BotSchedulars.GMT = await getGMT();
+    console.log('init schedulars with GMT value: ' + BotSchedulars.GMT);
     BotSchedulars.jobs.push(schedule.scheduleJob({ hour: (7 - BotSchedulars.GMT), minute: 30 }, birthdaySchedular)); // check for birthdays
     BotSchedulars.jobs.push(schedule.scheduleJob({ dayOfWeek: 4, hour: (13 - BotSchedulars.GMT), minute: 0 }, quoteSchedular));   // send weekly quote
     BotSchedulars.jobs.push(schedule.scheduleJob("*/10 * * * *", updateCoronaCountries));     // update corona data every 10 minutes
