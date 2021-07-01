6601c0716a1de28ae07d905a6628ba326959b986
Tomer Meirov <tomerm1@rafael.co.il>
2021-07-01 15:34:28 +0300

    shahar schedular improvment

--- src/controllers/schedulars.controller.ts
+++ src/controllers/schedulars.controller.ts
@@ -21,7 +21,9 @@ export async function initSchedulars() {
     console.log('init schedulars with GMT value: ' + BotSchedulars.GMT);
     BotSchedulars.jobs.push(schedule.scheduleJob({ hour: (7 - BotSchedulars.GMT), minute: 30 }, birthdaySchedular)); // check for birthdays
     BotSchedulars.jobs.push(schedule.scheduleJob({ dayOfWeek: 4, hour: (13 - BotSchedulars.GMT), minute: 0 }, quoteSchedular));   // send weekly quote
-    BotSchedulars.jobs.push(schedule.scheduleJob({ date: 1, hour: (10 - BotSchedulars.GMT), minute: 0 }, shaharSchedular));   // send SHAHAR reminder
+    BotSchedulars.jobs.push(schedule.scheduleJob({ date: 1, hour: (9 - BotSchedulars.GMT), minute: 0 }, shaharSchedular));   // send SHAHAR reminder
+    BotSchedulars.jobs.push(schedule.scheduleJob({ date: 2, hour: (9 - BotSchedulars.GMT), minute: 0 }, shaharSchedular));   // send SHAHAR reminder
+    BotSchedulars.jobs.push(schedule.scheduleJob({ date: 3, hour: (9 - BotSchedulars.GMT), minute: 0 }, shaharSchedular));   // send SHAHAR reminder
     BotSchedulars.jobs.push(schedule.scheduleJob("*/10 * * * *", updateCoronaCountries));     // update corona data every 10 minutes
     BotSchedulars.jobs.push(schedule.scheduleJob("*/10 * * * *", pingHeroku));                // ping own app to prevent idle
     BotSchedulars.jobs.push(schedule.scheduleJob({ hour: (5 - BotSchedulars.GMT), minute: 0 }, checkForGMTChanges));        // check for GMT changes
