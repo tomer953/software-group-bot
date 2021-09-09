a3f67e161069dc0ff5a61dc1adff6ed905e7fb6e
Tomer Meirov <tomerm1@rafael.co.il>
2021-09-09 08:00:06 +0300

    update birthday model

--- src/models/birthday.model.ts
+++ src/models/birthday.model.ts
@@ -3,6 +3,7 @@ import mongoose, { Schema } from 'mongoose'
 const schema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     name: String,
+    gender: { type: String, enum: ['female', 'male'], default: 'male' },
     birthday: String,
 }, { timestamps: true })
 
